f103742e1b8f6d738fae32e569c178b9af912965
“Gaia <st90070279279@gmail.com>
2026-02-13 00:33:53 +0800

    匯出型別

--- src/notify/index.ts
+++ src/notify/index.ts
@@ -1,4 +1,5 @@
 import Notifier from "./components/Notifier";
 
+export type { Notify, NotifyButton, NotifyButtonStyle } from "./types";
 export { notify } from "./core/actions";
 export { Notifier };
